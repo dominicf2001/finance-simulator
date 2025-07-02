95a070c18f247f33928ddb7dfd8c43b32eec0fbb
Dominic Ferrando <dominicf2001@gmail.com>
2025-07-02 09:25:34 -0400

    Init with basic account and transactions

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Account checking = new(10)
+{
+    Transactions = [
+        new Transaction(10),
+        new Transaction(5),
+        new Transaction(-2),
+    ]
+};
+
+Console.WriteLine(checking.GetBalance());
+
+public record Transaction(Decimal Amount);
+public class Account
+{
+    public Decimal InitialBalance;
+    public List<Transaction> Transactions = [];
+
+    public Account(Decimal initialBalance)
+    {
+        InitialBalance = initialBalance;
+    }
+
+    public Decimal GetBalance()
+    {
+        return InitialBalance + Transactions.Sum((t) => t.Amount);
+    }
+}
+
