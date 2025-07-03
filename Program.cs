d2aa60d10721fecd9d33cd6ae0dbe9a90fc6826a
Dominic Ferrando <dominicf2001@gmail.com>
2025-07-02 23:37:41 -0400

    Implement initial balance flag and do some formatting

--- Program.cs
+++ Program.cs
@@ -27,61 +27,85 @@ while (true)
                 Execute a command.
 
                 Commands:
-                    account-add <name> [options] 
+                    account-add <name> [options]    Create an account. 
+                    quit                            Quit the program.
 
                 Command Options:
-                    -h|--help   Show command help
+                    -h|--help   Show command help.
 
                 """);
             break;
         case "account-add":
+            // HELP
             if (input.Count() > 0 && new[] { "-h", "--help" }.Contains(input[1]))
             {
                 Console.Write("""
                         Description:
                             Create an account.
 
+                        Usage:
+                            account-add <name> [options]
+
                         Arguments:
-                            <name>  The account's name 
+                            <name>  The account's name.
 
                         Options:
-                            -h|--help               Show command help
-                            -i|--initial-balance    The account's initial balance 
+                            -h|--help               Show command help.
+                            -i|--initial-balance    The account's initial balance. 
 
                     """);
                 break;
             }
 
+            // ARGUMENTS
             if (input.Count() < 1 || CLI.isArgFlag(input[1]))
             {
                 Console.WriteLine("No account name provided.");
                 break;
             }
-
             string accountName = input[1];
-
-            List<CLI.Option> options = CLI.Parse.Options(input[2..]);
 
             Account account = new(accountName);
 
+            // OPTIONS
+            string? optionsError = null;
+            List<CLI.Option> options = CLI.Parse.Options(input[2..]);
             if (options.Count() > 0)
             {
-                // COMMAND LOGIC
                 foreach (CLI.Option option in options)
                 {
                     switch (option.Name)
                     {
                         case "--initial-balance":
                         case "-i":
+                            if (option.Value is null)
+                            {
+                                optionsError = "No initial balance amount provided.";
+                                break;
+                            }
+
+                            account.InitialBalance = Int32.Parse(option.Value);
+                            break;
+                        default:
+                            optionsError = $"Unknown option: {option.Name}";
                             break;
                     }
                 }
             }
+            // options error check 
+            if (optionsError is not null)
+            {
+                Console.WriteLine(optionsError);
+                break;
+            }
+
+            Console.WriteLine($"{account.Name}: ${account.GetBalance()}");
+
             break;
-        case "q":
+        case "quit":
             return;
         default:
-            Console.WriteLine("Unknown command");
+            Console.WriteLine($"Unknown command: {command}");
             break;
     }
 }
