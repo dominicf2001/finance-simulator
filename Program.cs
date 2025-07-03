c2f5d3b42f3d8aab1fd5db43aa4fc58bbf47e9ad
Dominic Ferrando <dominicf2001@gmail.com>
2025-07-02 22:22:08 -0400

    Implement help flags

--- Program.cs
+++ Program.cs
@@ -1,9 +1,66 @@
-﻿Account checking = new(10)
-{
-    Transactions = [
-        new Transaction(10),
-        new Transaction(5),
-    ]
-};
+﻿// ------------
+// INIT
+// ------------
+Console.Clear();
+Console.CancelKeyPress += (source, e) => { };
+// ------------
 
-Console.WriteLine(checking.GetBalance());
+// ------------
+// MAIN LOOP 
+// ------------
+while (true)
+{
+    Console.Write("> ");
+
+    string[] input = Console.ReadLine()?.Split(" ") ?? [];
+    if (input.Count() == 0)
+    {
+        Console.WriteLine("No command provided.");
+        continue;
+    }
+
+    string command = input[0];
+    switch (command)
+    {
+        case "help":
+            Console.Write("""
+                Execute a command.
+
+                Commands:
+                    account-add <name> [options] 
+
+                Command Options:
+                    -h|--help   Show command help
+
+            """);
+            break;
+        case "account-add":
+            string[] options = input[1..];
+            if (options.Count() > 0)
+            {
+                if (new[] { "-h", "--help" }.Contains(options[0]))
+                {
+                    Console.Write("""
+                        Description:
+                            Create an account.
+
+                        Arguments:
+                            <name>  The account's name 
+
+                        Options:
+                            -h|--help               Show command help
+                            -i|--initial-balance    The account's initial balance 
+
+                    """);
+                    break;
+                }
+            }
+            break;
+        case "q":
+            return;
+        default:
+            Console.WriteLine("Unknown command");
+            break;
+    }
+}
+// ------------
